15011024b6f29c0fded0c451ca1804f09f8da908
ruoru <cruoru@gmail.com>
2019-08-14 23:50:41 +0800

    update flask and vue {{}} different

--- app.py
+++ app.py
@@ -3,14 +3,20 @@ import json
 
 app = Flask(__name__, static_url_path='')
 
+
 @app.route('/', methods=['GET'])
 def home():
-  return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/add', methods=['POST'])
 def add():
-  j_data = json.loads(request.data)
-  return json.dumps({ 'data': j_data['a'] + j_data['b'] })
+    j_data = json.loads(request.data)
+    return json.dumps({
+        'answer': j_data['a'] + j_data['b'],
+        'questions': ['asjdhkjahs', 'asdkjhasdh']
+    })
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000, debug=True)
